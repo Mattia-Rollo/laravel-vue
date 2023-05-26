a420dc7e17ea20747fad7a7d99e0ba088fc19ba6
Mattia-Rollo <mattiarollo89@gmail.com>
2023-05-26 20:21:49 +0200

    created select input component

---
+++ resources/js/Components/SelectInput.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+
+defineProps({
+    modelValue: {
+        type: String,
+        options: Array,
+        required: true,
+    },
+    options: Array
+});
+
+defineEmits(['update:modelValue']);
+
+const input = ref(null);
+
+onMounted(() => {
+
+});
+
+// defineExpose({ focus: () => input.value.focus() });
+</script>
+
+<template>
+    <select
+        class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
+        :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" ref="input">
+        <option value="" selected></option>
+        <option v-for="(item, index) in options" :key="index" :value="item">{{ item }}</option>
+
+    </select>
+</template>
